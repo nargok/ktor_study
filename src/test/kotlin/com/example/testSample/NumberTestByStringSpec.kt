46fbc6e8a1ee3ebcf6a3497b0fb6513ca36789ce
nargok <nargok@gmail.com>
2022-04-02 19:38:10 +0900

    Test Data pattern

--- src/test/kotlin/com/example/testSample/NumberTestByStringSpec.kt
+++ src/test/kotlin/com/example/testSample/NumberTestByStringSpec.kt
@@ -1,6 +1,10 @@
 package com.example.testSample
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.headers
+import io.kotest.data.row
+import io.kotest.data.table
 import io.kotest.matchers.shouldBe
 
 class NumberTestByStringSpec : StringSpec() {
@@ -12,6 +16,30 @@ class NumberTestByStringSpec : StringSpec() {
         "isOdd:: when value is even number then return false" {
             val number = Number(2)
             number.isOdd() shouldBe false
+        }
+        "isRange:: when value in range then return true" {
+            forAll(
+                table(
+                    headers("value"),
+                    row(1),
+                    row(10)
+                )
+            ) { value ->
+                val number = Number(value)
+                number.isRange(1, 10) shouldBe true
+            }
+        }
+        "isRange:: when value not in range then return false" {
+            forAll(
+                table(
+                    headers("value"),
+                    row(0),
+                    row(11)
+                )
+            ) { value ->
+                val number = Number(value)
+                number.isRange(1, 10) shouldBe false
+            }
         }
     }
 }
